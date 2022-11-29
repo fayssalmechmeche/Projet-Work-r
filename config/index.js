d26a6e408b9c466099f302b219e43a0f125e61b1
fayssal <fayssalmechmeche.pro@gmail.com>
2022-11-29 23:11:40 +0100

    update of nodeJS

---
+++ config/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req,res)=> {
+    res.send('Hello World')
+})
+
+router.get('/dashboard', (req,res)=> {
+    res.send('dashboard')
+})
+
+
+router.post('/addUser',)
+module.exports= router
