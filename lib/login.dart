100304e551800e6c0a78ed03b666c958817def02
Yannis ROGEON <yayaro2711@gmail.com>
2022-11-30 11:49:58 +0100

    page de connexion edit

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 class login extends StatefulWidget {
   const login({Key? key, required String title}) : super(key: key);
@@ -16,7 +17,7 @@ class _loginState extends State<login> {
     return  Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.transparent,
-          leading: BackButton(
+          leading: const BackButton(
               color: Colors.black
           ),
           elevation: 0,
@@ -27,29 +28,55 @@ class _loginState extends State<login> {
           children: <Widget>[
             Column(
                 children: <Widget>[
-                  SizedBox(
+                  Container(
+                    width: 150,
+                    height: 150,
+                    child:  Image.asset("assets/logo.png"),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(top: 80),
                     width: 300,
                     child: TextFormField(
+                      cursorColor: Colors.grey,
                       controller: usernameController,
-                      decoration: const InputDecoration(
+                      decoration:  InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(90.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(90.0),
+                        ),
+                        contentPadding: const EdgeInsets.all(10),
                           labelText: "Nom d'utilisateur",
-                          icon: Icon(Icons.account_box)),
+                        labelStyle: TextStyle(color: Colors.grey),
+                     ),
                     ),
                   ),
-                  SizedBox(
+                  Container(
+                      padding: const EdgeInsets.only(top: 30),
                       width: 300,
                       child:TextFormField(
+
+                              cursorColor: Colors.grey,
                               controller: passwordController,
                               obscureText: !visiblePassword,
                               decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(90.0),
+                                ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: const BorderSide(color: Colors.grey),
+                                    borderRadius: BorderRadius.circular(90.0),
+                                  ),
                                 contentPadding: const EdgeInsets.all(10),
                                 labelText: "Mot de passe",
-                                icon: const Icon(Icons.lock),
+                                labelStyle: TextStyle(color: Colors.grey),
                                 suffixIcon: IconButton(
                                   icon: Icon(visiblePassword
                                       ? Icons.visibility_off
                                       : Icons.visibility),
-                                  color: Theme.of(context).primaryColorDark,
+                                  color: Colors.yellow,
                                   onPressed: () {
                                     setState(() {
                                       visiblePassword = !visiblePassword;
@@ -59,7 +86,29 @@ class _loginState extends State<login> {
                               ),
                             ),
                            ),
-                ]
+                  Container(
+                      padding: const EdgeInsets.only(top: 20),
+                      width: 150,
+                      height: 65,
+                      child: OutlinedButton(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          shape: const StadiumBorder(),
+                          foregroundColor: Colors.yellow,
+                          backgroundColor: Colors.yellow.withOpacity(0.5),
+
+                        ),
+                        child: const Text('Connexion', style: TextStyle(color: Colors.black)),
+                      )
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 15), child: GestureDetector(
+                    onTap: () {
+                      print('Text Clicked');
+                    },
+                    child: const Text('Mot de passe oublié ?'),
+                  ),)
+
+               ]
             ),
           ],
         ),
