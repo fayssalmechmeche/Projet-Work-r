e9bfd895d1d18587458a20605ff614bf2ce2db3c
fayssal <fayssalmechmeche.pro@gmail.com>
2022-12-01 21:51:37 +0100

    change localhost pour ios emulator

--- lib/Controller/NodeJSManager.dart
+++ lib/Controller/NodeJSManager.dart
@@ -2,40 +2,42 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class NodeJSManager {
-  static var url = "http://10.0.2.2:3000/";
+  static var url = "http://localhost:3000/";
 
-
-  static Future<int> createUser(String name,String password) async {
-  var response = await http.post(Uri.parse("${url}adduser"),headers:<String, String>{
-  'Content-Type': 'application/json; charset=UTF-8',
-  },body: jsonEncode(<String, String>{
-  'name': name,'password':password}));
-  print('Response status: ${response.statusCode}');
-  print('Response body: ${response.body}');
-  return response.statusCode;
-  }
-  static Future<int> authenticate(String name,String password) async {
-
-    var response = await http.post(Uri.parse("${url}authenticate"),headers:<String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },body: jsonEncode(<String, String>{
-      'name': name,'password':password}));
-
-   // print('Response status: ${response.statusCode}');
-   // print('Response body: ${response.body}');
-
+  static Future<int> createUser(String name, String password) async {
+    var response = await http.post(Uri.parse("${url}adduser"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{'name': name, 'password': password}));
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
     return response.statusCode;
   }
-  static Future<int> update(String name,String password) async{
-    var response = await http.post(Uri.parse("${url}update"),headers:<String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },body: jsonEncode(<String, String>{
-      'name': name,'password':password}));
+
+  static Future<int> authenticate(String name, String password) async {
+    var response = await http.post(Uri.parse("${url}authenticate"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{'name': name, 'password': password}));
 
     // print('Response status: ${response.statusCode}');
     // print('Response body: ${response.body}');
 
     return response.statusCode;
+  }
 
+  static Future<int> update(String name, String password) async {
+    var response = await http.post(Uri.parse("${url}update"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{'name': name, 'password': password}));
+
+    // print('Response status: ${response.statusCode}');
+    // print('Response body: ${response.body}');
+
+    return response.statusCode;
   }
 }
