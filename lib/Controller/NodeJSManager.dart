798008603ab18ae7f9176597fc757092ed201a84
fayssal <fayssalmechmeche.pro@gmail.com>
2022-12-01 15:07:38 +0100

    update authenticate method

--- lib/Controller/NodeJSManager.dart
+++ lib/Controller/NodeJSManager.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 class NodeJSManager {
   static var url = "http://10.0.2.2:3000/";
 
+
   static Future<void> createUser(String name,String password) async {
   var response = await http.post(Uri.parse("${url}adduser"),headers:<String, String>{
   'Content-Type': 'application/json; charset=UTF-8',
@@ -13,15 +14,16 @@ class NodeJSManager {
   print('Response body: ${response.body}');
   print( "good");
   }
-  static Future<void> authenticate(String name,String password) async {
+  static Future<int> authenticate(String name,String password) async {
 
     var response = await http.post(Uri.parse("${url}authenticate"),headers:<String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },body: jsonEncode(<String, String>{
       'name': name,'password':password}));
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
 
-    print( "good");
+   // print('Response status: ${response.statusCode}');
+   // print('Response body: ${response.body}');
+
+    return response.statusCode;
   }
 }
--- lib/view/login.dart
+++ lib/view/login.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:my_app/view/navigationpage.dart';
+
+import '../Controller/NodeJSManager.dart';
+
 class login extends StatefulWidget {
   const login({Key? key, required String title}) : super(key: key);
   static const tag = "/login";
@@ -14,103 +17,107 @@ class _loginState extends State<login> {
   var usernameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-
-    return  Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          leading: const BackButton(
-              color: Colors.black
-          ),
-          elevation: 0,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: const BackButton(color: Colors.black),
+        elevation: 0,
+      ),
       body: SafeArea(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Column(
-                children: <Widget>[
-                  Container(
-                    width: 150,
-                    height: 150,
-                    child:  Image.asset("assets/logo.png"),
+            Column(children: <Widget>[
+              Container(
+                width: 150,
+                height: 150,
+                child: Image.asset("assets/logo.png"),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 80),
+                width: 300,
+                child: TextFormField(
+                  cursorColor: Colors.grey,
+                  controller: usernameController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(90.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(90.0),
+                    ),
+                    contentPadding: const EdgeInsets.all(10),
+                    labelText: "Nom d'utilisateur",
+                    labelStyle: TextStyle(color: Colors.grey),
                   ),
-                  Container(
-                    padding: const EdgeInsets.only(top: 80),
-                    width: 300,
-                    child: TextFormField(
-                      cursorColor: Colors.grey,
-                      controller: usernameController,
-                      decoration:  InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(90.0),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.circular(90.0),
-                        ),
-                        contentPadding: const EdgeInsets.all(10),
-                          labelText: "Nom d'utilisateur",
-                        labelStyle: TextStyle(color: Colors.grey),
-                     ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 30),
+                width: 300,
+                child: TextFormField(
+                  cursorColor: Colors.grey,
+                  controller: passwordController,
+                  obscureText: !visiblePassword,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(90.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(90.0),
+                    ),
+                    contentPadding: const EdgeInsets.all(10),
+                    labelText: "Mot de passe",
+                    labelStyle: TextStyle(color: Colors.grey),
+                    suffixIcon: IconButton(
+                      icon: Icon(visiblePassword
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                      color: Colors.yellow,
+                      onPressed: () {
+                        setState(() {
+                          visiblePassword = !visiblePassword;
+                        });
+                      },
                     ),
                   ),
-                  Container(
-                      padding: const EdgeInsets.only(top: 30),
-                      width: 300,
-                      child:TextFormField(
+                ),
+              ),
+              Container(
+                  padding: const EdgeInsets.only(top: 20),
+                  width: 150,
+                  height: 65,
+                  child: OutlinedButton(
+                    onPressed: () async {
 
-                              cursorColor: Colors.grey,
-                              controller: passwordController,
-                              obscureText: !visiblePassword,
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(90.0),
-                                ),
-                                  focusedBorder: OutlineInputBorder(
-                                    borderSide: const BorderSide(color: Colors.grey),
-                                    borderRadius: BorderRadius.circular(90.0),
-                                  ),
-                                contentPadding: const EdgeInsets.all(10),
-                                labelText: "Mot de passe",
-                                labelStyle: TextStyle(color: Colors.grey),
-                                suffixIcon: IconButton(
-                                  icon: Icon(visiblePassword
-                                      ? Icons.visibility_off
-                                      : Icons.visibility),
-                                  color: Colors.yellow,
-                                  onPressed: () {
-                                    setState(() {
-                                      visiblePassword = !visiblePassword;
-                                    });
-                                  },
-                                ),
-                              ),
-                            ),
-                           ),
-                  Container(
-                      padding: const EdgeInsets.only(top: 20),
-                      width: 150,
-                      height: 65,
-                      child: OutlinedButton(
-                        onPressed: () {Navigator.of(context).pushNamed(navigationpage.tag);},
-                        style: OutlinedButton.styleFrom(
-                          shape: const StadiumBorder(),
-                          foregroundColor: Colors.yellow,
-                          backgroundColor: Colors.yellow.withOpacity(0.5),
+                      print(await NodeJSManager.authenticate(usernameController.text,
+                          passwordController.text));
+                      if (await NodeJSManager.authenticate(usernameController.text,
+                              passwordController.text) == 200) {
+                        Navigator.of(context).pushNamed(navigationpage.tag);
+                      };
 
-                        ),
-                        child: const Text('Connexion', style: TextStyle(color: Colors.black)),
-                      )
-                  ),
-                  Padding(padding: EdgeInsets.only(top: 15), child: GestureDetector(
-                    onTap: () {
-                      print('Text Clicked');
                     },
-                    child: const Text('Mot de passe oublié ?'),
-                  ),)
-
-               ]
-            ),
+                    style: OutlinedButton.styleFrom(
+                      shape: const StadiumBorder(),
+                      foregroundColor: Colors.yellow,
+                      backgroundColor: Colors.yellow.withOpacity(0.5),
+                    ),
+                    child: const Text('Connexion',
+                        style: TextStyle(color: Colors.black)),
+                  )),
+              Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: GestureDetector(
+                  onTap: () {
+                    print('Text Clicked');
+                  },
+                  child: const Text('Mot de passe oublié ?'),
+                ),
+              )
+            ]),
           ],
         ),
       ),
