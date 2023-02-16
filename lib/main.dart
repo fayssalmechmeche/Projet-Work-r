3f8297a79dbdaf13e96889861b38f6274a2c1691
Fayssal <f.mechmeche@epastelsoft.com>
2023-02-16 17:19:47 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const RegisterSelectionPage(),
+      home: const SelectionPage(),
       routes: {
         Login.tag: (context) => const Login(),
         LoginArt.tag: (context) => const LoginArt(),
