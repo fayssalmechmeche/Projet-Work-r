110305c9d958b84943ee71ed5ed272c0d2bb7ac5
Yannis ROGEON <yayaro2711@gmail.com>
2023-05-09 18:42:20 +0200

    add home artisan in navigation page

--- lib/view/Navigation/navigationpage.dart
+++ lib/view/Navigation/navigationpage.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:my_app/view/Home/homepageart.dart';
 import 'package:my_app/view/Profile/profile.dart';
 import 'package:my_app/view/Works/listwork.dart';
 import 'package:my_app/view/Msg/listchat.dart';
 import 'package:my_app/view/Home/homepage.dart';
 import 'package:my_app/view/ListDevis/ListProduction.dart';
+import 'package:provider/provider.dart';
+
+import '../../Controller/global.dart';
 
 class NavigationPage extends StatefulWidget {
   const NavigationPage({Key? key}) : super(key: key);
@@ -22,10 +26,19 @@ class _NavigationPageState extends State<NavigationPage> {
     ListWork(),
     Profile(title: "profile"),
   ];
+  static final List<Widget> _NavScreens2 = <Widget>[
+    ListChat(),
+    ListProposition(),
+    HomePageArt(),
+    ListWork(),
+    Profile(title: "profile"),
+  ];
   @override
   Widget build(BuildContext context) {
+     final globalData = Provider.of<GlobalData>(context);
     return Scaffold(
-        body: Center(child: _NavScreens.elementAt(_selectedIndex)),
+      
+        body: Center(child: (globalData.getRole() == 1) ? _NavScreens.elementAt(_selectedIndex) : _NavScreens2.elementAt(_selectedIndex)),
         bottomNavigationBar: GNav(
           gap: 8,
           tabBackgroundColor: Colors.yellow.withOpacity(0.5),
