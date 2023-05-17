4760411ef8183fb4218e8aed4ebaabc495dbb6af
fayssal <fayssalmechmeche.pro@gmail.com>
2023-05-17 23:21:19 +0200

    add a lot and a lot of verif for particulier

--- lib/view/Register/register.dart
+++ lib/view/Register/register.dart
@@ -282,17 +282,118 @@ class _RegisterPageState extends State<RegisterPage> {
                   if (emailValid == false) {
                     error = true;
                     const snackBar = SnackBar(
-                      content: Text(
-                          'Error in email ! please select a correct email'),
+                      content: Text('L\'adresse mail n\'est pas valide !'),
                     );
                     ScaffoldMessenger.of(context).showSnackBar(snackBar);
                   }
+
+                  if (cityController.text.contains(new RegExp(r'[0-9]'))) {
+                    error = true;
+                    const snackBar = SnackBar(
+                      content: Text(
+                          'Le nom de la ville ne doit pas contenir de chiffre !'),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+
+                  if (PostalCodeController.text.length != 5) {
+                    error = true;
+                    const snackBar = SnackBar(
+                      content:
+                          Text('Le code postale doit contenir 5 chiffres !'),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+                  if (telephoneController.text.length != 10) {
+                    error = true;
+                    const snackBar = SnackBar(
+                      content:
+                          Text('Le numero de telephone n\'est pas valide !'),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+
+                  if (telephoneController.text[0] != "0") {
+                    error = true;
+                    const snackBar = SnackBar(
+                      content:
+                          Text('Le numero de téléphone doit commencer par 0 !'),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+
+                  if (firstnameController.text.contains(new RegExp(r'[0-9]'))) {
+                    error = true;
+                    const snackBar = SnackBar(
+                      content:
+                          Text('Le prenom ne doit pas contenir de chiffre !'),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+                  if (nameController.text.contains(new RegExp(r'[0-9]'))) {
+                    error = true;
+                    const snackBar = SnackBar(
+                      content: Text('Le nom ne doit pas contenir de chiffre !'),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+
+                  if (passwordController.text.length < 8) {
+                    error = true;
+                    const snackBar = SnackBar(
+                      content: Text(
+                          'Le mot de passe doit contenir au moins 8 caractères !'),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+
                   if (passwordController.text !=
                       confirmPasswordController.text) {
                     error = true;
                     const snackBar = SnackBar(
                       content: Text(
                           'La confirmation de mot de passe et le mot de passe ne sont pas identique !'),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+
+                  if (passwordController.text.contains(new RegExp(r'[a-z]')) ==
+                      false) {
+                    error = true;
+                    const snackBar = SnackBar(
+                      content: Text(
+                          'Le mot de passe doit contenir au moins une minuscule !'),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+
+                  if (passwordController.text.contains(new RegExp(r'[A-Z]')) ==
+                      false) {
+                    error = true;
+                    const snackBar = SnackBar(
+                      content: Text(
+                          'Le mot de passe doit contenir au moins une majuscule !'),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+
+                  if (passwordController.text.contains(new RegExp(r'[0-9]')) ==
+                      false) {
+                    error = true;
+                    const snackBar = SnackBar(
+                      content: Text(
+                          'Le mot de passe doit contenir au moins un chiffre !'),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+
+                  if (passwordController.text
+                          .contains(new RegExp(r'[!@#$%^&*(),.?":{}|<>]')) ==
+                      false) {
+                    error = true;
+                    const snackBar = SnackBar(
+                      content: Text(
+                          'Le mot de passe doit contenir au moins un caractère spécial !'),
                     );
                     ScaffoldMessenger.of(context).showSnackBar(snackBar);
                   }
